c2907e5ee647cc98b2437ab6f394c6760414182a
Denis Voloshin <d.voloshin@infosec.ru>
2017-10-11 17:11:11 +0300

    fixed wallie's comments

--- lang_frequency.py
+++ lang_frequency.py
@@ -3,21 +3,21 @@ from collections import Counter
 
 def load_data(filepath):
     f = open(filepath, "r")
-    text = f.read()
+    file_content = f.read()
     f.close()
-    return text
+    return file_content
 
 
 def get_most_frequent_words(text):
-    split_text = str.split(text)
+    words_list = str.split(text)
     words_to_print = 10
-    return Counter(split_text).most_common(words_to_print)
+    return Counter(words_list).most_common(words_to_print)
 
 
 if __name__ == '__main__':
     filename = input()
-    text = load_data(filename)
-    frequent_words = get_most_frequent_words(text)
+    file_content = load_data(filename)
+    frequent_words = get_most_frequent_words(file_content)
     # Counter.most_common() returns array of pairs (a,b) a - element, b - count, therefore calling word[0] i take
     # element from this pair. its not 'magic number'
     print([word[0] for word in frequent_words])
