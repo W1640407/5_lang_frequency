f50647aa5ad42375d1d25f7a4516438d022f00cf
Denis Voloshin <d.voloshin@infosec.ru>
2017-10-12 11:45:38 +0300

    fixed wallie's comments

--- README.md
+++ README.md
@@ -1,8 +1,26 @@
 # Frequency Analysis of Words
 
-Script counts all words in file given through stdin and prints 10 most frequent of them
-input: filename with filepath
-output: array of words separated by comma
+Выводит 10 наиболее часто встречающихся в файле слов в убывающем порядке.  
+Имя фала передается аргументом командной строки
+
+####Запуск скрипта:
+```
+python lang_frequency.py <filename>
+```
+где  `<filename>` - имя файла
+
+####Вывод:
+Массив из 10 наиболее часто встречающихся в файле слов в убывающем порядке
+
+####Пример запуска:
+```
+python lang_frequency.py test.txt
+```
+####Пример вывода:
+```
+10 наиболее часто встречающихся в тексте слов, по убыванию:
+['e', 'a', 'd', 'f', 'g', 'h', 'i', 'j', 'c', 'b']
+```
 
 # Project Goals
 
--- lang_frequency.py
+++ lang_frequency.py
@@ -1,21 +1,24 @@
+import sys
 from collections import Counter
 
 
 def load_data(filepath):
-    file_reader = open(filepath, "r")
-    file_content = file_reader.read()
-    file_reader.close()
+    with open(filepath, "r") as file_reader:
+        file_content = file_reader.read()
     return file_content
 
 
 def get_most_frequent_words(text):
-    words_list = str.split(text)
-    words_to_print = 10
-    return Counter(words_list).most_common(words_to_print)
+    words_list = text.split()
+    words_amount_to_print = 10
+    return Counter(words_list).most_common(words_amount_to_print)
 
 
 if __name__ == '__main__':
-    filename = input()
+    filename = sys.argv[1]
     file_content = load_data(filename)
     frequent_words = get_most_frequent_words(file_content)
-    print([word[0] for word in frequent_words])
+    # Counter.most_common() returns array of pairs (a,b) where a - element, b - count, therefore calling pair[0] returns
+    # element from this pair.
+    print("10 наиболее часто встречающихся в тексте слов, по убыванию:")
+    print([pair[0] for pair in frequent_words])
